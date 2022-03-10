62799728605168fc4615da0aaa02b52be6339892
ashishbakliwal7 <ashishbakliwal7@gmail.com>
2022-03-11 00:16:03 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,10 @@ mongoose.connect(
   }
 );
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log(`Server running on port 5000`);
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+
+app.listen(port, host, function() {
+  console.log("Server started.......");
 });
+
